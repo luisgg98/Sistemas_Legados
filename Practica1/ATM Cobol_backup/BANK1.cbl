e5d6d7c2639ba8e07c89a86964206c4a5dd14527
Luis Garcia <luisgarcia98@hotmail.es>
2020-10-04 19:02:35 +0200

    Reintentar clave Se ha arreglado y ya funciona cuando intentas poner la clave bien cuando te has equivocado. EL programa ahora se bloquea completamente cuando bloqueas la tarjeta.

--- Practica1/ATM Cobol_backup/BANK1.cbl
+++ Practica1/ATM Cobol_backup/BANK1.cbl
@@ -64,14 +64,16 @@
            05 DIF-GMT              PIC S9(4).
 
        01 KEYBOARD-STATUS           PIC 9(4).
-           88 ENTER-PRESSED          VALUE 0.
+           88 ENTER-PRESSED          VALUE 0000.
            88 PGUP-PRESSED        VALUE 2001.
            88 PGDN-PRESSED        VALUE 2002.
            88 UP-ARROW-PRESSED    VALUE 2003.
            88 DOWN-ARROW-PRESSED  VALUE 2004.
            88 ESC-PRESSED         VALUE 2005.
+           88 F1-PRESSED         VALUE  1001.
+           88 F2-PRESSED         VALUE  1002.
 
-       77 PRESSED-KEY              PIC  9(4).
+       77 PRESSED-KEY  BLANK WHEN ZERO  PIC  9(4).
        77 PIN-INTRODUCIDO          PIC  9(4).
        *> 0 RIGHT CORNER NOT SHOWING:
        77 CHOICE BLANK WHEN ZERO   PIC  9(1).
@@ -96,7 +98,6 @@
            SET ENVIRONMENT 'COB_SCREEN_ESC'        TO 'Y'
 
            DISPLAY BLANK-SCREEN.
-
            DISPLAY "Cajero Automatico UnizarBank" AT LINE 2 COL 26
                WITH FOREGROUND-COLOR IS BLUE.
 
@@ -122,7 +123,7 @@
                 AT LINE 24 COL 33.
 
        P1-ENTER. *> Estaba en AT LINE 24 COL 80
-           ACCEPT CHOICE OFF AT LINE 24 COL 80 ON EXCEPTION
+           ACCEPT PRESSED-KEY OFF AT LINE 24 COL 80 ON EXCEPTION
            IF ENTER-PRESSED
                GO TO P2
            ELSE
@@ -181,7 +182,7 @@
                AT LINE 13 COL 15.
            DISPLAY "7 - Comprar entradas de espectaculos"
                AT LINE 14 COL 15.
-           DISPLAY "8 - Cambiar clave" AT LINE 16 COL 15.
+           DISPLAY "8 - Cambiar clave" AT LINE 15 COL 15.
            DISPLAY "ESC - Salir" AT LINE 24 COL 34.
 
        PMENUA1. *> Estaba en 24 80) (
@@ -192,37 +193,28 @@
                    GO TO PMENUA1.
 
            IF CHOICE = 1
-               CALL "BANK2" USING TNUM
-               GO TO PMENU.
+               CALL "BANK2" USING TNUM.
 
            IF CHOICE = 2
-               CALL "BANK3" USING TNUM
-               GO TO PMENU.
+               CALL "BANK3" USING TNUM.
 
            IF CHOICE = 3
-               CALL "BANK4" USING TNUM
-               GO TO PMENU.
-
+               CALL "BANK4" USING TNUM.
            IF CHOICE = 4
-               CALL "BANK5" USING TNUM
-               GO TO PMENU.
+               CALL "BANK5" USING TNUM.
 
            IF CHOICE = 5
-               CALL "BANK6" USING TNUM
-               GO TO PMENU.
-
+               CALL "BANK6" USING TNUM.
            IF CHOICE = 6
-               CALL "BANK9" USING TNUM
-               GO TO PMENU.
+               CALL "BANK9" USING TNUM.
 
            IF CHOICE = 7
-               CALL "BANK7" USING TNUM
-               GO TO PMENU.
+               CALL "BANK7" USING TNUM.
 
            IF CHOICE = 8
-               CALL "BANK8" USING TNUM
-               GO TO PMENU.
+               CALL "BANK8" USING TNUM.
 
+           PERFORM CALCULAR-INTENTOS THRU CALCULAR-INTENTOS.
            GO TO PMENU.
 
 
@@ -234,11 +226,11 @@
            PERFORM IMPRIMIR-CABECERA THRU IMPRIMIR-CABECERA.
            DISPLAY "Ha ocurrido un error interno"
                AT LINE 9 COL 25
-               WITH FOREGROUND-COLOR IS BLACK
+               WITH FOREGROUND-COLOR IS WHITE
                    BACKGROUND-COLOR IS RED.
            DISPLAY "Vuelva mas tarde"
                AT LINE 11 COL 32
-               WITH FOREGROUND-COLOR IS BLACK
+               WITH FOREGROUND-COLOR IS WHITE
                     BACKGROUND-COLOR IS RED.
            DISPLAY "Enter - Aceptar" AT LINE 24 COL 33.
 
@@ -253,26 +245,27 @@
            PERFORM IMPRIMIR-CABECERA THRU IMPRIMIR-CABECERA.
            DISPLAY "Se ha sobrepasado el numero de intentos"
                AT LINE 9 COL 20
-               WITH FOREGROUND-COLOR IS BLACK
+               WITH FOREGROUND-COLOR IS WHITE
                     BACKGROUND-COLOR IS RED.
            DISPLAY "Por su seguridad se ha bloqueado la tarjeta"
                AT LINE 11 COL 18
-               WITH FOREGROUND-COLOR IS BLACK
+               WITH FOREGROUND-COLOR IS WHITE
                     BACKGROUND-COLOR IS RED.
                     
            DISPLAY "Acuda a una sucursal"
                AT LINE 12 COL 30
-               WITH FOREGROUND-COLOR IS BLACK
+               WITH FOREGROUND-COLOR IS WHITE
                     BACKGROUND-COLOR IS RED.
 
            DISPLAY "Enter - Aceptar" AT LINE 24 COL 33.
 
        PINT-ERR-ENTER.
-       *>AT LINE 24 COL 80
-           ACCEPT CHOICE AT LINE 24 COL 79 ON EXCEPTION 
-           IF ENTER-PRESSED
+       *>AT LINE 24 COL 80 
+       *>https://youtu.be/4MB0CmrADaU
+           ACCEPT PRESSED-KEY OFF AT LINE 24 COL 80 ON EXCEPTION 
+           IF ENTER-PRESSED THEN
                GO TO IMPRIMIR-CABECERA
-           ELSE
+           ELSE 
                GO TO PINT-ERR-ENTER.
 
 
@@ -286,20 +279,20 @@
            PERFORM IMPRIMIR-CABECERA THRU IMPRIMIR-CABECERA.
            DISPLAY "El codigo PIN es incorrecto"
                AT LINE 9 COL 26
-               WITH FOREGROUND-COLOR IS BLACK
+               WITH FOREGROUND-COLOR IS WHITE
                     BACKGROUND-COLOR IS RED.
            DISPLAY"Le quedan "
                AT LINE 11 COL 30
-               WITH FOREGROUND-COLOR IS BLACK
+               WITH FOREGROUND-COLOR IS WHITE
                     BACKGROUND-COLOR IS RED.
            DISPLAY IINTENTOS
                AT LINE 11 COL 40
-               WITH FOREGROUND-COLOR IS BLACK
+               WITH FOREGROUND-COLOR IS WHITE
                     BACKGROUND-COLOR IS RED.
 
            DISPLAY " intentos"
                AT LINE 11 COL 42 
-               WITH FOREGROUND-COLOR IS BLACK
+               WITH FOREGROUND-COLOR IS WHITE
                     BACKGROUND-COLOR IS RED.
 
            DISPLAY "Enter - Aceptar" AT LINE 24 COL 1.
@@ -307,16 +300,29 @@
 
        PPIN-ERR-ENTER.
        *> Estaba en 24 80
-           ACCEPT CHOICE AT LINE 24 COL 79 ON EXCEPTION
+           ACCEPT PRESSED-KEY OFF AT LINE 24 COL 80 ON EXCEPTION
            IF ENTER-PRESSED
                GO TO P2
            ELSE
                IF ESC-PRESSED
                    GO TO IMPRIMIR-CABECERA
                ELSE
-                   GO TO PPIN-ERR-ENTER.
-
-
+                   GO TO PPIN-ERR-ENTER
+           END-IF.
        REINICIAR-INTENTOS.
            MOVE 3 TO IINTENTOS.
            REWRITE INTENTOSREG INVALID KEY GO TO PSYS-ERR.
+
+       CALCULAR-INTENTOS.
+           OPEN I-O INTENTOS.
+           IF FSI NOT = 00 THEN
+               GO TO PSYS-ERR.
+           MOVE TNUM TO INUM.
+
+           READ INTENTOS INVALID KEY GO TO PSYS-ERR.
+
+           IF IINTENTOS = 0 THEN
+               GO TO IMPRIMIR-CABECERA.
+              
+           CLOSE INTENTOS.
+     
