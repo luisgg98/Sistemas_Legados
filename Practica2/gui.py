a160c1c343d9044646be5c8b07ce6d32820346a3
Alberto Calvo <albertocalvojaca@gmail.com>
2020-10-26 22:59:27 +0100

    Pantalla login

--- Practica2/gui.py
+++ Practica2/gui.py
@@ -6,6 +6,21 @@ import re
 import unidecode
 
 
+def fecha_correcta(fecha):
+    match = re.match('^(0[1-9]|[12][0-9]|3[01])[/](0[1-9]|1[012])[/](19|20)\d\d$', fecha)
+    if match is None:
+        return False
+    else:
+        return True
+
+
+def comprobar_campos_login(user, password):
+    if len(user) == 0 or len(password) == 0:
+        return False
+    else:
+        return True
+
+
 class Gui:
     """
     Interfaz de la aplicación legada de tareas en un mainframe. Esta se conecta al mainframe y
@@ -25,45 +40,70 @@ class Gui:
         """
         # Iniciar wrapper e interfaz
         self.wr.iniciar()
-        layout = self.__get_layout()
-        window = sg.Window('UnizarTasks', layout, font=self.FONT)
 
-        # Lectura de eventos en la app
+        # Inicio de sesion
+        login_win = self.__get_login_win()
+        login_correcto = False
         while True:
-            event, values = window.read()
-            if event == sg.WIN_CLOSED or event == 'Salir':
+            event, values = login_win.read()
+            if event == sg.WIN_CLOSED:
                 break
-            elif event == 'Nueva tarea':
-                add_task_window = self.__new_task_win()
-                while True:
-                    event2, values2 = add_task_window.read()
-                    if event2 == sg.WIN_CLOSED:
+            elif event == 'Iniciar sesion':
+                user, password = values['-USUARIO-'], values['-CONTRASEÑA-']
+                if comprobar_campos_login(user, password):
+                    if self.__login(user, password):
+                        login_correcto = True
                         break
-                    elif event2 == event2 == '-NOMBRE-' and len(values2['-NOMBRE-']) > 4:
-                        add_task_window['-NOMBRE-'].update(values2['-NOMBRE-'][0:4])
-                    elif event2 == '-DESCRIPCION-' and len(values2['-DESCRIPCION-']) > 12:
-                        add_task_window['-DESCRIPCION-'].update(values2['-DESCRIPCION-'][0:12])
-                    elif event2 == '-FECHA-':
-                        if len(values2['-FECHA-']) > 10:
-                            add_task_window['-FECHA-'].update(values2['-FECHA-'][0:10])
-                    elif event2 == 'Añadir':
-                        fecha, nombre, descripcion = values2['-FECHA-'], values2['-NOMBRE-'], values2['-DESCRIPCION-']
-                        # Comprobar campos vacios y correctos
-                        if len(descripcion) == 0 or len(fecha) < 10 or not self.fecha_correcta(fecha):
-                            sg.popup("Introduce correctamente la fecha dd/mm/yyyy y descripcion")
-                            continue
-                        nombre = unidecode.unidecode(nombre)
-                        descripcion = unidecode.unidecode(descripcion)
-                        # Escribir en mainframe
-                        self.__write_task(fecha, nombre, descripcion)
-                        # Mostrar cambios
-                        self.__reload_table(window)
-                        break
+                    else:
+                        login_win['-USUARIO-'].update('')
+                        login_win['-CONTRASEÑA-'].update('')
+                        sg.popup('ERROR: Usuario y/o contraseña incorrectos')
+                else:
+                    sg.popup('ERROR: Los campos no pueden ser vacios')
+        login_win.close()
 
-                add_task_window.close()  # Fin de nueva tarea
+        if login_correcto:
+            main_win = self.__get__main_win()
 
+            # Lectura de eventos en la app
+            while True:
+                event, values = main_win.read()
+                if event == sg.WIN_CLOSED or event == 'Salir':
+                    break
+                elif event == 'Nueva tarea':
+                    add_task_main_win = self.__new_task_win()
+                    while True:
+                        event2, values2 = add_task_main_win.read()
+                        if event2 == sg.WIN_CLOSED:
+                            break
+                        elif event2 == event2 == '-NOMBRE-' and len(values2['-NOMBRE-']) > 4:
+                            add_task_main_win['-NOMBRE-'].update(values2['-NOMBRE-'][0:4])
+                        elif event2 == '-DESCRIPCION-' and len(values2['-DESCRIPCION-']) > 12:
+                            add_task_main_win['-DESCRIPCION-'].update(values2['-DESCRIPCION-'][0:12])
+                        elif event2 == '-FECHA-':
+                            if len(values2['-FECHA-']) > 10:
+                                add_task_main_win['-FECHA-'].update(values2['-FECHA-'][0:10])
+                        elif event2 == 'Añadir':
+                            fecha, nombre, descripcion = values2['-FECHA-'], values2['-NOMBRE-'], values2[
+                                '-DESCRIPCION-']
+                            # Comprobar campos vacios y correctos
+                            if len(descripcion) == 0 or len(fecha) < 10 or not fecha_correcta(fecha):
+                                sg.popup("Introduce correctamente la fecha dd/mm/yyyy y descripcion")
+                                continue
+                            nombre = unidecode.unidecode(nombre)
+                            descripcion = unidecode.unidecode(descripcion)
+                            # Escribir en mainframe
+                            self.__write_task(fecha, nombre, descripcion)
+                            # Mostrar cambios
+                            self.__reload_table(main_win)
+                            break
+
+                    add_task_main_win.close()  # Fin de nueva tarea
+            main_win.close()
         self.wr.exitMainFrame()
-        window.close()
+
+    def __login(self, user, password):
+        return self.wr.login(user, password)
 
     def __write_task(self, fecha, nombre, descripcion):
         """ Escribir tarea en mainframe """
@@ -84,7 +124,29 @@ class Gui:
             tasks = list(map(lambda x: x[1:], tasks))  # Eliminar numero de tarea
         return tasks
 
-    def __get_layout(self):
+    def __get_login_win(self):
+        login_layout = [
+            [
+                sg.Column(
+                    [
+                        [sg.Text('Usuario')],
+                        [sg.Text('Contraseña')]
+                    ]
+                ),
+                sg.Column(
+                    [
+                        [sg.Input(key='-USUARIO-')],
+                        [sg.Input(key='-CONTRASEÑA-', password_char='*')],
+                    ]
+                )
+            ],
+            [
+                sg.Button('Iniciar sesion')
+            ]
+        ]
+        return sg.Window('Inicio de sesión', login_layout, font=self.FONT)
+
+    def __get__main_win(self):
         """ Devuelve el layout general para crear la interfaz """
         data = self.__get_tasks()
         col1 = [[sg.Text('Mis tareas')],
@@ -101,7 +163,7 @@ class Gui:
 
         col2 = [[sg.Button('Nueva tarea')], [sg.Exit('Salir')]]
         layout = [[sg.Col(col1, ), sg.Col(col2)]]
-        return layout
+        return sg.Window('UnizarTasks', layout, font=self.FONT)
 
     def __new_task_win(self):
         """ Devuelve Window correspondiente a la pantalla de creación de tareas"""
@@ -135,13 +197,6 @@ class Gui:
         # Obtener tareas
         data = self.__get_tasks()
         window['-TABLE-'].update(data)
-
-    def fecha_correcta(self, fecha):
-        match = re.match('^(0[1-9]|[12][0-9]|3[01])[/](0[1-9]|1[012])[/](19|20)\d\d$', fecha)
-        if match is None:
-            return False
-        else:
-            return True
 
 
 gui = Gui()
