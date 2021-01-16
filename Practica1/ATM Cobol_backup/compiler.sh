2e5f47f88af723d17e452ab70f4b6ba0af954a2b
Luis Garcia <luisgarcia98@hotmail.es>
2021-01-16 11:16:57 +0100

    Refactoring files

--- Practica1/ATM Cobol_backup/compiler.sh
+++ Practica1/ATM Cobol_backup/compiler.sh
@@ -1,6 +1,4 @@
-#cobc -x escribir.cbl
-#./escribir
-#rm -f escribir
+#!/bin/bash
 rm -f BANK1
 cobc -x  BANK1.cbl BANK2.cbl BANK3.cbl BANK4.cbl BANK5.cbl BANK6.cbl BANK7.cbl BANK8.cbl BANK9.cbl PERIOD_BANK.cbl 
 ./BANK1
